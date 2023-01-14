c225b8465291e0382239fa88ee24a81c6d1118ef
Hassan <mhassandata@gmail.com>
2023-01-14 22:28:04 +0500

    Updated numpy.py

--- Numpy/numpy.py
+++ Numpy/numpy.py
@@ -1,9 +1,17 @@
 #Numpy
 """
+What does Numpy means and what are Numpy can do
 1. Numerical Python
 2. Numerical Array
 3. Fastest
 4. Vectorized Operation
 5 Contigious Memory
-
+6. Numpy is an array
 """
+#import numpy as np
+import numpy as np
+x = np.array([22,33,44,55])
+print(x)
+print(x.size)
+print(x.shape)
+print(x.ndim)
